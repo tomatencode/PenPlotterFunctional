cacaf8064d7e9e36394aa927c60dd06822561228
tomatencode <ingo_thomas@gmx.de>
2026-02-27 18:19:18 +0100

    setup form main coded

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+#include <TMCStepper.h>
+#include "motion/Stepper.hpp"
+#include "motion/CoreXYPlanner.hpp"
+#include "motion/CoreXYKinematics.hpp"
+#include "motion/StepperAxis.hpp"
 
 // === Workspace dimensions ===
 #define maxX_mm 185.0f
@@ -15,9 +20,41 @@
 #define DRIVER_RX_PIN 16
 #define R_SENSE 0.11f
 
+HardwareSerial driverSerial(1);
+
+TMC2209Stepper driverA(&driverSerial, R_SENSE, 0);
+TMC2209Stepper driverB(&driverSerial, R_SENSE, 2);
+Stepper stepA(STEP_PIN_A, DIR_PIN_A);
+Stepper stepB(STEP_PIN_B, DIR_PIN_B);
+StepperAxis axisA(stepA, [](){ return 16.0f; });
+StepperAxis axisB(stepB, [](){ return 16.0f; });
+
 
 void setup() {
-  // put your setup code here, to run once:
+  // == MUTE BUZZER ==
+  pinMode(14, OUTPUT);
+  digitalWrite(14, LOW);
+
+  // === ENABLE DRIVERS ===
+  pinMode(EN_PIN, OUTPUT);
+  digitalWrite(EN_PIN, LOW);
+
+  driverSerial.begin(115200, SERIAL_8N1, DRIVER_RX_PIN, DRIVER_TX_PIN);
+  // === INIT DRIVER A ===
+  driverA.begin();
+  driverA.toff(5);
+  driverA.rms_current(1000);
+  driverA.microsteps(16);
+  driverA.en_spreadCycle(false);
+  driverA.pwm_autoscale(true);
+
+  // === INIT DRIVER B ===
+  driverB.begin();
+  driverB.toff(5);
+  driverB.rms_current(1000);
+  driverB.microsteps(16);
+  driverB.en_spreadCycle(false);
+  driverB.pwm_autoscale(true);
 }
 
 void loop() {
