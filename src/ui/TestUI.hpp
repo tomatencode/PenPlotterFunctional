fe48c61cdade4ed7a21e923da2266b62e896220d
tomatencode <ingo_thomas@gmx.de>
2026-03-11 17:12:15 +0100

    scrollable container implemented but not working

--- src/ui/TestUI.hpp
+++ src/ui/TestUI.hpp
@@ -9,6 +9,7 @@
 #include "text/textSources/StaticText.hpp"
 #include "widgets/ProgressBarWidget.hpp"
 #include "text/textSources/FunctionText.hpp"
+#include "widgetSystem/ScrollableContainer.hpp"
 
 // Global screen pointers to keep them alive
 Screen* g_screen1 = nullptr;
@@ -40,14 +41,24 @@ static void goBack()
     if (g_router) g_router->popScreen();
 }
 
-static void onButton1Pressed()
+static void onProgPlusPressed()
 {
     g_progressValue = (g_progressValue >= 90) ? 100 : g_progressValue + 10; // Increment progress, max at 100
 }
 
-static void onButton2Pressed()
+static void onProgMinusPressed()
 {
     g_progressValue = (g_progressValue >= 10) ? (g_progressValue - 10) : 0; // Decrement progress, floor at 0
+}
+
+static void onButton1Pressed()
+{
+    Serial.println("Button 1 pressed");
+}
+
+static void onButton2Pressed()
+{
+    Serial.println("Button 2 pressed");
 }
 
 static void onButton3Pressed()
@@ -59,6 +70,17 @@ static void onButton4Pressed()
 {
     Serial.println("Button 4 pressed");
 }
+
+static void onButton5Pressed()
+{
+    Serial.println("Button 5 pressed");
+}
+
+static void onButton6Pressed()
+{
+    Serial.println("Button 6 pressed");
+}
+
 
 static uint8_t getProgressValue()
 {
@@ -81,7 +103,7 @@ void setupTestUI(Router& router)
 
     static StaticText submenu1BtnText("Submenu 1");
     LabelWidget* sub1Label = new LabelWidget({0, 0, 18, 1}, submenu1BtnText);
-    ButtonWidget* button1 = new ButtonWidget(
+    ButtonWidget* sub1Button = new ButtonWidget(
                                                 {0, 1, 20, 1}, sub1Label, defaultButtonStyle,
                                                 nullptr,
                                                 goToScreen2,
@@ -90,14 +112,14 @@ void setupTestUI(Router& router)
 
     static StaticText submenu2BtnText("Submenu 2");
     LabelWidget* sub2Label = new LabelWidget({0, 0, 18, 1}, submenu2BtnText);
-    ButtonWidget* button2 = new ButtonWidget(
+    ButtonWidget* sub2Button = new ButtonWidget(
                                                 {0, 2, 20, 1}, sub2Label, defaultButtonStyle,
                                                 nullptr,
                                                 goToScreen3,
                                                 {HorizontalAlignment::Center, VerticalAlignment::Top}
                                             );
 
-    Widget* screen1Widgets[] = { label1, button1, button2 };
+    Widget* screen1Widgets[] = { label1, sub1Button, sub2Button };
     g_screen1 = new Screen(screen1Widgets, 3);
 
     // ---------------- Submenu 1 ----------------
@@ -109,17 +131,17 @@ void setupTestUI(Router& router)
     static FunctionText progressText(progressTextFunc);
     LabelWidget* progressLabel = new LabelWidget({15, 2, 5, 1}, progressText, {HorizontalAlignment::Right, VerticalAlignment::Top});
 
-    static StaticText btn1Text("+ prog");
-    LabelWidget* btn1Label = new LabelWidget({0, 0, 8, 1}, btn1Text);
-    ButtonWidget* button3 = new ButtonWidget({0, 3, 10, 1}, btn1Label, defaultButtonStyle,
-                                             onButton1Pressed,
+    static StaticText ProgPlusText("+ prog");
+    LabelWidget* ProgPlusLabel = new LabelWidget({0, 0, 8, 1}, ProgPlusText);
+    ButtonWidget* ProgPlusButton = new ButtonWidget({0, 3, 10, 1}, ProgPlusLabel, defaultButtonStyle,
+                                             onProgPlusPressed,
                                              nullptr,
                                              {HorizontalAlignment::Center, VerticalAlignment::Top});
 
-    static StaticText btn2Text("- prog");
-    LabelWidget* btn2Label = new LabelWidget({0, 0, 8, 1}, btn2Text);
-    ButtonWidget* button4 = new ButtonWidget({10, 3, 10, 1}, btn2Label, defaultButtonStyle,
-                                             onButton2Pressed,
+    static StaticText ProgMinusText("- prog");
+    LabelWidget* ProgMinusLabel = new LabelWidget({0, 0, 8, 1}, ProgMinusText);
+    ButtonWidget* ProgMinusButton = new ButtonWidget({10, 3, 10, 1}, ProgMinusLabel, defaultButtonStyle,
+                                             onProgMinusPressed,
                                              nullptr,
                                              {HorizontalAlignment::Center, VerticalAlignment::Top});
 
@@ -130,30 +152,54 @@ void setupTestUI(Router& router)
                                                    nullptr,
                                                    goBack);
 
-    Widget* screen2Widgets[] = { label2, progressBar, progressLabel, button3, button4, back_button_1 };
+    Widget* screen2Widgets[] = { label2, progressBar, progressLabel, ProgPlusButton, ProgMinusButton, back_button_1 };
     g_screen2 = new Screen(screen2Widgets, 6);
 
     // ---------------- Submenu 2 ----------------
     static StaticText submenu2TitleText("Submenu 2");
     LabelWidget* label3 = new LabelWidget({0, 0, 13, 1}, submenu2TitleText);
 
+    static StaticText btn1Text("Button 1");
+    LabelWidget* btn1Label = new LabelWidget({0, 0, 18, 1}, btn1Text);
+    ButtonWidget* button1 = new ButtonWidget({0, 0, 20, 1}, btn1Label, defaultButtonStyle,
+                                             onButton1Pressed);
+
+    static StaticText btn2Text("Button 2");
+    LabelWidget* btn2Label = new LabelWidget({0, 0, 18, 1}, btn2Text);
+    ButtonWidget* button2 = new ButtonWidget({0, 1, 20, 1}, btn2Label, defaultButtonStyle,
+                                             onButton2Pressed);
+
     static StaticText btn3Text("Button 3");
     LabelWidget* btn3Label = new LabelWidget({0, 0, 18, 1}, btn3Text);
-    ButtonWidget* button5 = new ButtonWidget({0, 2, 20, 1}, btn3Label, defaultButtonStyle,
+    ButtonWidget* button3 = new ButtonWidget({0, 2, 20, 1}, btn3Label, defaultButtonStyle,
                                              onButton3Pressed);
 
     static StaticText btn4Text("Button 4");
     LabelWidget* btn4Label = new LabelWidget({0, 0, 18, 1}, btn4Text);
-    ButtonWidget* button6 = new ButtonWidget({0, 3, 20, 1}, btn4Label, defaultButtonStyle,
+    ButtonWidget* button4 = new ButtonWidget({0, 3, 20, 1}, btn4Label, defaultButtonStyle,
                                              onButton4Pressed);
+
+    static StaticText btn5Text("Button 5");
+    LabelWidget* btn5Label = new LabelWidget({0, 0, 18, 1}, btn5Text);
+    ButtonWidget* button5 = new ButtonWidget({0, 4, 20, 1}, btn5Label, defaultButtonStyle,
+                                             onButton5Pressed);
+
+    static StaticText btn6Text("Button 6");
+    LabelWidget* btn6Label = new LabelWidget({0, 0, 18, 1}, btn6Text);
+    ButtonWidget* button6 = new ButtonWidget({0, 5, 20, 1}, btn6Label, defaultButtonStyle,
+                                             onButton6Pressed);
+
+
+    Widget* scrollableChildren[] = { button1, button2, button3, button4, button5, button6 };
+    ScrollableContainer* scrollableContainer = new ScrollableContainer({0, 2, 20, 2}, {HorizontalAlignment::Left, VerticalAlignment::Top}, scrollableChildren, 6);
 
     LabelWidget* backLabel2 = new LabelWidget({0, 0, 6, 1}, backText);
     ButtonWidget* back_button_2 = new ButtonWidget({14, 0, 6, 1}, backLabel2, defaultButtonStyle,
                                                    nullptr,
                                                    goBack);
 
-    Widget* screen3Widgets[] = { label3, button5, button6, back_button_2 };
-    g_screen3 = new Screen(screen3Widgets, 4);
+    Widget* screen3Widgets[] = { label3, scrollableContainer, back_button_2 };
+    g_screen3 = new Screen(screen3Widgets, 3);
 
     router.setScreen(g_screen1);
 }
