e57a81a5d1403a88c21ba6b74622d27d3c2adc1e
tomatencode <ingo_thomas@gmx.de>
2026-03-20 17:22:27 +0100

    value selector working

--- src/ui/framework/widgets/leaves/ValueSelector.hpp
+++ src/ui/framework/widgets/leaves/ValueSelector.hpp
@@ -1,9 +1,7 @@
 #pragma once
 
-#include <cstdio>
 #include <functional>
-#include <limits>
-#include <string>
+#include <sstream>
 #include <type_traits>
 
 #include "../core/SelectableWidget.hpp"
@@ -14,8 +12,8 @@ namespace widgets {
 
 struct ValueSelectorStyle
 {
-    Glyph leftNormal   = '(';
-    Glyph rightNormal  = ')';
+    Glyph leftNormal   = GLYPH_NONE;
+    Glyph rightNormal  = GLYPH_NONE;
     Glyph leftFocused  = '<';
     Glyph rightFocused = '>';
     Glyph leftPressed  = '-';
@@ -31,157 +29,37 @@ struct ValueSelectorStyle
 
 // ValueSelector provides a focusable widget for +/− selection using rotary encoder and button.
 // Typical styles:
-// not focused -> (3)
+// not focused -> 3
 // focused -> <3>
 // pressed -> -3-
 // focused+editing -> >3<
-
-// Formatter helper, avoids invalid type_traits instantiation for non-enum when T is integral/float.
-template <typename U, typename Enable = void>
-struct ValueSelectorFormatter
-{
-    static std::string format(const U&) { return std::string(); }
-};
-
-template <typename U>
-struct ValueSelectorFormatter<U, typename std::enable_if<std::is_enum<U>::value>::type>
-{
-    static std::string format(const U& value)
-    {
-        using Underlying = typename std::underlying_type<U>::type;
-        return std::to_string(static_cast<Underlying>(value));
-    }
-};
-
-template <typename U>
-struct ValueSelectorFormatter<U, typename std::enable_if<std::is_integral<U>::value>::type>
-{
-    static std::string format(const U& value)
-    {
-        return std::to_string(value);
-    }
-};
-
-template <typename U>
-struct ValueSelectorFormatter<U, typename std::enable_if<std::is_floating_point<U>::value>::type>
-{
-    static std::string format(const U& value)
-    {
-        char buffer[32];
-        std::snprintf(buffer, sizeof(buffer), "%.3g", static_cast<double>(value));
-        return std::string(buffer);
-    }
-};
-
-// Clamping helper
-template <typename U, typename Enable = void>
-struct ValueSelectorClamper
-{
-    static void clamp(U&, const U&, const U&) {}
-};
-
-template <typename U>
-struct ValueSelectorClamper<U, typename std::enable_if<std::is_enum<U>::value>::type>
-{
-    static void clamp(U& value, const U& minValue, const U& maxValue)
-    {
-        using Underlying = typename std::underlying_type<U>::type;
-        auto v = static_cast<Underlying>(value);
-        auto mn = static_cast<Underlying>(minValue);
-        auto mx = static_cast<Underlying>(maxValue);
-        if (v < mn) v = mn;
-        if (v > mx) v = mx;
-        value = static_cast<U>(v);
-    }
-};
-
-template <typename U>
-struct ValueSelectorClamper<U, typename std::enable_if<std::is_arithmetic<U>::value>::type>
-{
-    static void clamp(U& value, const U& minValue, const U& maxValue)
-    {
-        if (value < minValue) value = minValue;
-        if (value > maxValue) value = maxValue;
-    }
-};
-
-// Encoder update helper
-template <typename U, typename Enable = void>
-struct ValueSelectorUpdater
-{
-    static void update(U&, const U&, const U&, const U&, int) {}
-};
-
-template <typename U>
-struct ValueSelectorUpdater<U, typename std::enable_if<std::is_enum<U>::value>::type>
-{
-    static void update(U& value, const U& step, const U& minValue, const U& maxValue, int delta)
-    {
-        using Underlying = typename std::underlying_type<U>::type;
-        auto v = static_cast<Underlying>(value);
-        auto s = static_cast<Underlying>(step);
-        v += static_cast<Underlying>(delta) * s;
-        auto mn = static_cast<Underlying>(minValue);
-        auto mx = static_cast<Underlying>(maxValue);
-        if (v < mn) v = mn;
-        if (v > mx) v = mx;
-        value = static_cast<U>(v);
-    }
-};
-
-template <typename U>
-struct ValueSelectorUpdater<U, typename std::enable_if<std::is_arithmetic<U>::value>::type>
-{
-    static void update(U& value, const U& step, const U& minValue, const U& maxValue, int delta)
-    {
-        value += static_cast<U>(delta) * step;
-        if (value < minValue) value = minValue;
-        if (value > maxValue) value = maxValue;
-    }
-};
 
 template <typename T>
 class ValueSelector : public SelectableWidget
 {
 public:
-    using Formatter = std::function<std::string(const T&)>;
-    using ChangeCallback = std::function<void(const T&)>;
-
     ValueSelector(
         T initialValue,
-        T minValue,
-        T maxValue,
-        T stepValue = static_cast<T>(1),
-        ValueSelectorStyle style = ValueSelectorStyle{},
-        Formatter formatter = nullptr,
-        ChangeCallback onChange = nullptr
+        std::function<T(T)> next,
+        std::function<T(T)> prev,
+        std::function<void(const T&)> onChange = nullptr,
+        std::function<std::string(const T&)> ToString = nullptr,
+        ValueSelectorStyle style = ValueSelectorStyle{}
     )
         : _value(initialValue)
-        , _minValue(minValue)
-        , _maxValue(maxValue)
-        , _step(stepValue)
+        , _next(std::move(next))
+        , _prev(std::move(prev))
+        , _onChange(std::move(onChange))
+        , _ToString(std::move(ToString))
         , _style(style)
-        , _formatter(formatter ? formatter : defaultFormatter)
-        , _onChange(onChange)
     {
-        clampValue();
-    }
-
-    void setValue(const T& value)
-    {
-        _value = value;
-        clampValue();
-        invokeCallback();
     }
 
     const T& value() const { return _value; }
 
-    void setEditing(bool editing) { _isEditing = editing; }
-    bool isEditing() const { return _isEditing; }
-
     Size measure() const override
     {
-        std::string text = formatValue();
+        std::string text = _ToString(_value);
         int width = static_cast<int>(text.size());
 
         if (_isEditing)
@@ -204,15 +82,15 @@ public:
         Glyph left = GLYPH_NONE;
         Glyph right = GLYPH_NONE;
 
-        if (_isEditing)
-        {
-            left = _style.leftEditing;
-            right = _style.rightEditing;
-        }
-        else if (_isPressed)
+        if (_isPressed)
         {
             left = _style.leftPressed;
             right = _style.rightPressed;
+        }
+        else if (_isEditing)
+        {
+            left = _style.leftEditing;
+            right = _style.rightEditing;
         }
         else if (isFocused())
         {
@@ -234,7 +112,7 @@ public:
             x++;
         }
 
-        std::string text = formatValue();
+        std::string text = _ToString(_value);
         int remaining = canvasBox.w - (x - canvasBox.x);
         if (right.code != GLYPH_NONE.code && remaining > 0)
             remaining--; // leave space for right marker
@@ -259,17 +137,15 @@ public:
         if (!isFocused())
             return;
 
+        _isPressed = input.buttonState.buttonDown;
+
         if (input.buttonState.buttonPressed)
         {
             // Toggle edit mode on button press
             _isEditing = !_isEditing;
 
-            input.buttonState.buttonPressed = false;
-            if (!_isEditing)
-                _isPressed = false;
+            input.buttonState.buttonPressed = false; // consume the event so parent does not also act on it
         }
-
-        _isPressed = input.buttonState.buttonDown;
 
         if (_isEditing && input.encoderDelta != 0)
         {
@@ -284,45 +160,24 @@ public:
 
 private:
     T _value;
-    T _minValue;
-    T _maxValue;
-    T _step;
+    std::function<T(T)> _next;
+    std::function<T(T)> _prev;
+    std::function<void(const T&)> _onChange;
+    std::function<std::string(const T&)> _ToString;
+
     bool _isEditing = false;
     bool _isPressed = false;
 
     ValueSelectorStyle _style;
-    Formatter _formatter;
-    ChangeCallback _onChange;
-
-    static std::string defaultFormatter(const T& value)
-    {
-        return ValueSelectorFormatter<T>::format(value);
-    }
-
-    std::string formatValue() const
-    {
-        if (_formatter)
-            return _formatter(_value);
-        return defaultFormatter(_value);
-    }
-
-    void clampValue()
-    {
-        ValueSelectorClamper<T>::clamp(_value, _minValue, _maxValue);
-    }
 
     void updateValueByEncoder(int delta)
     {
-        if (delta == 0)
-            return;
+        if (delta > 0)
+            _value = _next(_value);
+        else if (delta < 0)
+            _value = _prev(_value);
 
-        ValueSelectorUpdater<T>::update(_value, _step, _minValue, _maxValue, delta);
-        invokeCallback();
-    }
-
-    void invokeCallback()
-    {
-        if (_onChange)
+        if (delta != 0 && _onChange)
             _onChange(_value);
     }
 };
--- src/ui/screens/SecondaryScreen.cpp
+++ src/ui/screens/SecondaryScreen.cpp
@@ -26,7 +26,12 @@ SecondaryScreen::SecondaryScreen()
             ),
             widgets::make_layout<widgets::HorizontalLayout>(widgets::LayoutStyle(),
                 widgets::make_widget<widgets::LabelWidget>("Value: "),
-                widgets::make_widget<widgets::ValueSelector<int>>(5, 0, 10)
+                widgets::make_widget<widgets::ValueSelector<int>>(5,
+                    [](int val) { return val + 1; }, // next value (stub)
+                    [](int val) { return val - 1; }, // prev value (stub)
+                    [](const int& newValue) { /* onChange callback (stub) */ },
+                    [](const int& val) { return std::to_string(val); } // ToString function
+                )
             )
 
         )
