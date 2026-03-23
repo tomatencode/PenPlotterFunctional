af95eb10142fa5d5f36d6f7ab91258e51e771ec0
tomatencode <ingo_thomas@gmx.de>
2026-03-23 20:09:19 +0100

    Glyph string working

--- src/ui/framework/text/GlyphString.hpp
+++ src/ui/framework/text/GlyphString.hpp
@@ -21,6 +21,14 @@ public:
             _data.emplace_back(static_cast<uint8_t>(text[i]));
     }
 
+    GlyphString(const char* text) {
+        size_t n = 0;
+        while (text[n] != '\0') ++n;
+        _data.reserve(n + 1);
+        for (size_t i = 0; i < n; ++i)
+            _data.emplace_back(static_cast<uint8_t>(text[i]));
+    }
+
     size_t size() const { return _data.size(); }
 
     Glyph operator[](size_t i) const { return i < size() ? _data[i] : GLYPH_NONE; }
