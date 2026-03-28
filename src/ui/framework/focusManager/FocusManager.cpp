be6f8cea02550ff5bb0a5dbec7e548872cda5ee7
tomatencode <ingo_thomas@gmx.de>
2026-03-28 16:28:34 +0100

    fixed focus manager refresh bug

--- src/ui/framework/focusManager/FocusManager.cpp
+++ src/ui/framework/focusManager/FocusManager.cpp
@@ -7,16 +7,18 @@ FocusManager::FocusManager() : _widgets(), _selecedIndx(0) {}
 void FocusManager::setWidgets(std::vector<ui::widgets::Selectable*> widgets, uint8_t firstFocused)
 {
     _widgets = std::move(widgets);
-    _selecedIndx = firstFocused % _widgets.size(); // Ensure firstFocused is within bounds
 
-    if (_widgets.empty())
-        return;
+    if (_widgets.empty()) return;
+
+    firstFocused = std::min(static_cast<size_t>(firstFocused), _widgets.size() - 1);
+
+    _selecedIndx = firstFocused;
     
     // find the first enabled widget to focus on
     while (_widgets[_selecedIndx] == nullptr || !_widgets[_selecedIndx]->isEnabled())
     {
         _selecedIndx = (_selecedIndx + 1) % _widgets.size();
-        if (_selecedIndx == firstFocused % _widgets.size()) return; // Avoid infinite loop if all widgets are null or hidden
+        if (_selecedIndx == firstFocused) return; // Avoid infinite loop if all widgets are null or hidden
     }
 
     _widgets[_selecedIndx]->focus();
@@ -85,10 +87,17 @@ void FocusManager::refresh() {
     if (_widgets.empty()) return;
     
     // if current valid, skip
-    if (_selecedIndx < _widgets.size() && _widgets[_selecedIndx] && _widgets[_selecedIndx]->isEnabled()) return;
+    if (_selecedIndx < _widgets.size() && _widgets[_selecedIndx] && _widgets[_selecedIndx]->isEnabled()) {
+        if (!_widgets[_selecedIndx]->isFocused()) {
+            _widgets[_selecedIndx]->focus();
+        }
+        return;
+    };
+
     if (_selecedIndx < _widgets.size() && _widgets[_selecedIndx]) _widgets[_selecedIndx]->unfocus();
 
-    size_t startIndex = _selecedIndx;
+    size_t startIndex = std::min(_selecedIndx, _widgets.size() - 1);
+
     while (_widgets[_selecedIndx] == nullptr || !_widgets[_selecedIndx]->isEnabled())
     {
         _selecedIndx = (_selecedIndx + _widgets.size() - 1) % _widgets.size();
