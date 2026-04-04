33f271bb14b7fb4ed2b1ff9aa7d5bf5ffae60396
tomatencode <ingo_thomas@gmx.de>
2026-04-04 12:19:55 +0200

    fixed bug with std::string conversion

--- src/settings/SettingsRepository.cpp
+++ src/settings/SettingsRepository.cpp
@@ -188,12 +188,10 @@ void SettingsRepository::loadNetworkSettings() {
     prefs.begin("network", true);  // true = read-only mode
 
     // Use hardcoded config header values as defaults on first boot
-    String ssid = prefs.getString("ssid", SSID);
-    _network.ssid = ssid.c_str();
-    String password = prefs.getString("password", PASSWORD);
-    _network.password = password.c_str();
-    String mdnsName = prefs.getString("mdnsName", MDNS_NAME);
-    _network.mdnsName = mdnsName.c_str();
+    // Construct std::string from c_str() (data is copied into std::string)
+    _network.ssid = std::string(prefs.getString("ssid", SSID).c_str());
+    _network.password = std::string(prefs.getString("password", PASSWORD).c_str());
+    _network.mdnsName = std::string(prefs.getString("mdnsName", MDNS_NAME).c_str());
 
     prefs.end();
 }
